4e6860d27a36daff73c287effb1bb3471ac0408e
Guilherme Leao <leaob7@gmail.com>
2022-04-11 15:04:11 -0300

    interface vehicle ok

---
+++ src/interfaces/VehicleInterface.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const VehicleSchema = z.object({
+  model: z.string()
+    .min(3, { message: 'Model must have at least 3 characters' }),
+  year: z.number().gte(1900, { message: 'Year must be graten than 1900' }),
+  color: z.string()
+    .min(3, { message: 'Color must have at least 3 characters' }),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type Vehicle = z.infer<typeof VehicleSchema>;
